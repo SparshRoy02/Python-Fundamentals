e85a8e704ed8ccc926ab0bb978ecce7edb43ee69
SparshRoy02 <sparshroy2002@gmail.com>
2026-02-01 22:22:08 +0530

    Add hello.py with basic Python print examples

---
+++ 01_Introduction/hello.py
@@ -0,0 +1,29 @@
+print("Hello World!")                 # print Hello World
+                         
+print("Hello World!")                 # each call prints text on a new line by default
+print("I am learning Python.")
+print("It is awesome!")
+
+print("This will work!")              # double quotes
+print('This will also work!')         # single quotes
+
+
+print("Hello World!", end=" ")        # print without new line
+print("I will print on same line.")
+
+print("Hello"); print("How are you?"); print("Bye bye!")   # multiple statements on one line by separating with semicolon(;)
+
+
+print(3)                  # print numbers
+print(358)
+print(50000)
+
+print(3 + 3)              # addition
+print(3 - 2)              # subtraction
+print(2 * 5)              # multiplication
+print(5 / 2)              # division
+print(3 ** 2)             # exponential
+print(3 % 2)              # modulus
+print(3 // 2)             # Floor division operator
+
+print("I am", 35, "years old.")      # mix text and numbers
